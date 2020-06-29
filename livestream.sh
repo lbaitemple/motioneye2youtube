6858fb26a8cef3b7e4c544ba4eb542607213f833
lbaitemple <lbai@temple.edu>
2020-06-29 14:26:11 -0400

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -2,4 +2,4 @@
 
 ip=$1
 key=$2
-ffmpeg  -thread_queue_size 512 -re -f mjpeg -i "$ip" -f lavfi -i anullsrc -c:v libx264 -g 60 -c:a aac -ar 44100 -ac 2 -f flv  "rtmp://a.rtmp.youtube.com/live2/$key"
+ffmpeg  -hide_banner -loglevel panic -thread_queue_size 512 -re -f mjpeg -i "$ip" -f lavfi -i anullsrc -c:v libx264 -g 60 -c:a aac -ar 44100 -ac 2 -f flv  "rtmp://a.rtmp.youtube.com/live2/$key"
